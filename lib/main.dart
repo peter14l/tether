2c06cc41fb6598b34dca9518ada1f0abc27b099a
Peter <peter_parker_2008@outlook.com>
2026-04-14 02:25:01 +0530

    feat: integrate Sentry SDK for error tracking and telemetry in main entry point

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ import 'injection_container.dart';
 
 import 'dart:ui';
 import 'package:firebase_core/firebase_core.dart';
-import 'core/telemetry/telemetry_service.dart';
+import 'core/telemetry/telemetry_service.dart';import 'package:sentry_flutter/sentry_flutter.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -47,12 +48,25 @@ Future<void> main() async {
     return true;
   };
 
-  runApp(
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = 'https://bc1753eb3948e1640ebca0654b5a7bce@o4511214683881472.ingest.us.sentry.io/4511214685388800';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+      // The sampling rate for profiling is relative to tracesSampleRate
+      // Setting to 1.0 will profile 100% of sampled transactions:
+      options.profilesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(SentryWidget(child: 
     BlocProvider(
       create: (context) => getIt<AuthBloc>()..add(AuthCheckRequested()),
       child: const MyApp(),
     ),
+  )),
   );
+  // TODO: Remove this line after sending the first sample event to sentry.
+  await Sentry.captureException(Exception('This is a sample exception.'));
 }
 
 class MyApp extends StatelessWidget {
