ea51d7a33d0d9a266bfdfb9b2dc39455204013c3
Peter <peter_parker_2008@outlook.com>
2026-04-14 02:15:59 +0530

    feat: migrate build pipeline to use --dart-define-from-file and compile-time constants

--- lib/core/config/env_config.dart
+++ lib/core/config/env_config.dart
@@ -8,4 +8,24 @@ class EnvConfig {
     'SUPABASE_PUBLISHABLE_KEY',
     defaultValue: 'sb_publishable_nIbV5_x67pr_UAa9nx9pxA_tp9nGG3z',
   );
+
+  static const String razorpayKeyId = String.fromEnvironment(
+    'RAZORPAY_KEY_ID',
+    defaultValue: '',
+  );
+
+  static const String razorpayKeySecret = String.fromEnvironment(
+    'RAZORPAY_KEY_SECRET',
+    defaultValue: '',
+  );
+
+  static const String sentryDsn = String.fromEnvironment(
+    'SENTRY_DSN',
+    defaultValue: '',
+  );
+
+  static const String appEnv = String.fromEnvironment(
+    'APP_ENV',
+    defaultValue: 'development',
+  );
 }
